81b1fedfd1b556470c4456f277384599465f8a4c
Greg Daughtry <greg.daughtry@xilinx.com>
2014-07-18 16:44:10 -0600

    check in zynq bd generation script

--- scripts/setup.tcl
+++ scripts/setup.tcl
@@ -12,10 +12,10 @@ set_property "board_part" "xilinx.com:zc702:part0:1.0" $obj
 set_property "simulator_language" "Mixed" $obj
 set_property "target_language" "Verilog" $obj
 
-add_files -norecurse $thisDir/../hdl/top/threeFlop.v
+add_files -norecurse $thisDir/../hdl/threeFlop/threeFlop.v
 add_files -norecurse $thisDir/../xdc//top.xdc
-#add_files -norecurse $thisDir/../ip/axi_chip2chip_0/axi_chip2chip_0.xci
-#update_compile_order -fileset sources_1
+add_files -norecurse $thisDir/../ip/axi_chip2chip_0/axi_chip2chip_0.xci
+update_compile_order -fileset sources_1
 #set_property SOURCE_SET sources_1 [get_filesets sim_1]
 #add_files -fileset sim_1 -norecurse $thisDir/../tb/hdl_zynq/tb.v
 #update_compile_order -fileset sim_1
