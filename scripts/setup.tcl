36a4c01062f7b945e2853670a8ade99035ead3d2
Greg Daughtry <greg.daughtry@xilinx.com>
2014-07-18 17:31:44 -0600

    added in ipi subsystem generation and instantiation at top level

--- scripts/setup.tcl
+++ scripts/setup.tcl
@@ -12,12 +12,15 @@ set_property "board_part" "xilinx.com:zc702:part0:1.0" $obj
 set_property "simulator_language" "Mixed" $obj
 set_property "target_language" "Verilog" $obj
 
-add_files -norecurse $thisDir/../hdl/threeFlop/threeFlop.v
-add_files -norecurse $thisDir/../xdc//top.xdc
-add_files -norecurse $thisDir/../ip/axi_chip2chip_0/axi_chip2chip_0.xci
+add_files -norecurse ../hdl/top/top.v
+add_files -norecurse ../hdl/threeFlop/threeFlop.v
+add_files -norecurse ../xdc/top.xdc
+add_files -norecurse ../ip/axi_chip2chip_0/axi_chip2chip_0.xci
+add_files -norecurse ../dsp/module_1_ext/module_1.slx
+add_files -norecurse ../bd/zynq_1/zynq_1.bd
 update_compile_order -fileset sources_1
 #set_property SOURCE_SET sources_1 [get_filesets sim_1]
-#add_files -fileset sim_1 -norecurse $thisDir/../tb/hdl_zynq/tb.v
+#add_files -fileset sim_1 -norecurse ../tb/hdl_zynq/tb.v
 #update_compile_order -fileset sim_1
 
 # if everything is successful "touch" a file so make will not it's done
