2860d6e01ce112a121f1d9c93465f531f55dcf44
Greg Daughtry <greg.daughtry@xilinx.com>
2014-07-21 16:52:03 -0600

    renaming zynq bd

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,2 +1,15 @@
-call c:\Xilinx\Vivado\2014.2\settings64.bat
-export PATH=/C/Xilinx/Vivado_HLS/2014.2/msys/bin:$PATH
+# All of this is from settings64.bat - made to work for bash
+VER="2014.2"
+
+# call c:\Xilinx\Vivado\2014.2\settings64.bat
+# which calls:
+
+# CALL C:\Xilinx\Vivado\2014.2\.settings64-Vivado.bat
+export PATH=/c/Xilinx/Vivado/$VER/bin:/c/Xilinx/Vivado/$VER/lib/win64.o:$PATH
+export XILINX_VIVADO=/c/Xilinx/Vivado/$VER
+
+# CALL C:\Xilinx\Vivado_HLS\2014.2\.settings64-Vivado_High_Level_Synthesis.bat
+# CALL C:\Xilinx\SDK\2014.2\.settings64-Software_Development_Kit.bat
+
+# now set up mingw
+export PATH=/c/Xilinx/Vivado_HLS/$VER/msys/bin:$PATH
