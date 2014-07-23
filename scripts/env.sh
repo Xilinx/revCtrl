7d1fb7f03ba51d010573346d93ebdbddb8743f63
Greg Daughtry <greg.daughtry@xilinx.com>
2014-07-23 14:18:26 -0600

    added vivado env setup script for bash

--- scripts/env.sh
+++ scripts/env.sh
@@ -9,7 +9,10 @@ export PATH=/c/Xilinx/Vivado/$VER/bin:/c/Xilinx/Vivado/$VER/lib/win64.o:$PATH
 export XILINX_VIVADO=/c/Xilinx/Vivado/$VER
 
 # CALL C:\Xilinx\Vivado_HLS\2014.2\.settings64-Vivado_High_Level_Synthesis.bat
+export PATH=/c/Xilinx/Vivado_HLS/$VER/bin:$PATH
+
 # CALL C:\Xilinx\SDK\2014.2\.settings64-Software_Development_Kit.bat
+export PATH=/c/Xilinx/SDK/$VER/bin:/c/Xilinx/SDK/$VER/gnu/microblaze/nt/bin:/c/Xilinx/SDK/$VER/gnu/arm/nt/bin:/c/Xilinx/SDK/$VER/gnu/microblaze/linux_toolchain/nt64_be/bin:/c/Xilinx/SDK/$VER/gnu/microblaze/linux_toolchain/nt64_le/bin:$PATH
 
 # now set up mingw
 export PATH=/c/Xilinx/Vivado_HLS/$VER/msys/bin:$PATH
