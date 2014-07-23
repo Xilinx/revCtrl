8ab8dc79747eb71a9771be6e0e3bebe76dcb0436
Greg Daughtry <greg.daughtry@xilinx.com>
2014-07-23 17:47:10 -0600

    swapped axi iic ip module

--- ip/axi_iic_0/axi_iic_0_stub.vhdl
+++ ip/axi_iic_0/axi_iic_0_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2014.2 (win64) Build 932637 Wed Jun 11 13:33:10 MDT 2014
--- Date        : Wed Jul 23 14:26:08 2014
+-- Date        : Wed Jul 23 17:36:56 2014
 -- Host        : XCODAUGHTRY30 running 64-bit Service Pack 1  (build 7601)
 -- Command     : write_vhdl -force -mode synth_stub c:/junk/tsc14/work/ip/axi_iic_0/axi_iic_0_stub.vhdl
 -- Design      : axi_iic_0
