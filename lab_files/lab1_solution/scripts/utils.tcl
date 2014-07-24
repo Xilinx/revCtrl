7b2ef272b185fed9c12d66cc4251ceb533a0836f
Ron Plyler <rplyler@xilinx.com>
2014-07-24 15:34:41 -0700

    removed lab1_solution

--- lab_files/lab1_solution/scripts/utils.tcl
+++
@@ -1,8 +0,0 @@
-# this is a collection of useful project utilities
-
-# implement touch - opens a file updating the time stamp,
-# creating it if it does not exist
-proc touch {f} {
-   set FILEIN [open $f w]
-   close $FILEIN
-}
