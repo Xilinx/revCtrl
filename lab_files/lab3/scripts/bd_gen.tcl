c43dd8f1dad7711458a3ed1ac83c89c7d61dcb3b
Ron Plyler <rplyler@xilinx.com>
2014-07-31 18:51:14 -0700

    updated lab 3

--- lab_files/lab3/scripts/bd_gen.tcl
+++ lab_files/lab3/scripts/bd_gen.tcl
@@ -3,19 +3,8 @@ set thisDir [file dirname [info script]]
 # source common utilities
 source $thisDir/utils.tcl
 
-# Create project
-create_project zynq ./zynq -part xc7z020clg484-1
-
-# Set the directory path for the new project
-set proj_dir [get_property directory [current_project]]
-
-# Source the bd.tcl file to create the bd
+# Recreate the block design
 source $thisDir/bd.tcl
-validate_bd_design
-save_bd_design
 
-# Generate Target
-generate_target all [get_files */zynq_bd.bd]
-
-# if everything is successful "touch" a file so make will not it's done
+# if everything is successful "touch" a file so make will note it's done
 touch {.bd_gen.done}
