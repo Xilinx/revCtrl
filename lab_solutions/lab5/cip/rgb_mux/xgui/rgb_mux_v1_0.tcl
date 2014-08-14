b008054b3a33e519c53de362912cce07d9da28bd
Ron Plyler <rplyler@xilinx.com>
2014-08-14 16:30:32 -0700

    Removing the extra files. Somehow they got merged back in.

--- lab_solutions/lab5/cip/rgb_mux/xgui/rgb_mux_v1_0.tcl
+++
@@ -1,8 +0,0 @@
-# Definitional proc to organize widgets for parameters.
-proc init_gui { IPINST } {
-	set Page0 [ipgui::add_page $IPINST -name "Page 0" -layout vertical]
-	set canvas_spec [ipgui::get_canvasspec -of $IPINST]
-	set_property ip_logo "misc/logo.png" $canvas_spec
-	set Component_Name [ipgui::add_param $IPINST -parent $Page0 -name Component_Name]
-}
-
