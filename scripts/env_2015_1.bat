12d82fa6e5a7820a922346072be5c1d2f467c488
Greg Daughtry <greg.daughtry@xilinx.com>
2015-01-14 11:42:33 -0700

    updated flow for 2015.1

--- scripts/env_2015_1.bat
+++ scripts/env_2015_1.bat
@@ -1,2 +1,2 @@
-call c:\Xilinx\Vivado\2014.2\settings64.bat
-SET PATH=C:\Xilinx\Vivado_HLS\2014.2\msys\bin;%PATH%
+call c:\Xilinx\Vivado\2015.1\settings64.bat
+SET PATH=C:\Xilinx\Vivado_HLS\2015.1\msys\bin;%PATH%
