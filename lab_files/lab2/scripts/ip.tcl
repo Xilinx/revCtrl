375d88ec784c900636ccc88a7d879ce2d5683674
Greg Daughtry <greg.daughtry@xilinx.com>
2015-01-28 12:33:14 -0700

    removed lab files and solutions - now just source data

--- lab_files/lab2/scripts/ip.tcl
+++
@@ -1,8 +0,0 @@
-# get the directory where this script resides
-set thisDir [file dirname [info script]]
-# source common utilities
-source $thisDir/utils.tcl
-
-# script will be sourced from work directory
-# output created in ip directory
-set ipDir ./ip
