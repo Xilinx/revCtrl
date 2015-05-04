d2dc89c02f2640463ac340cd9ef0a46d0764207f
Greg Daughtry <greg.daughtry@xilinx.com>
2015-05-04 16:02:29 -0600

    fixed port width mismatch in iic wrapper

--- hdl/top/iicWrapper.v
+++ hdl/top/iicWrapper.v
@@ -27,7 +27,7 @@ module iicWrapper(
     input shiftr
     );
     
-    parameter size = 111;
+    parameter size = 109;
     wire [size-1:0] f;
     wire [size-1:0] s_f;
     
@@ -50,27 +50,27 @@ module iicWrapper(
       .s_axi_awaddr(f[10:2]),    // input wire [8 : 0] s_axi_awaddr
       .s_axi_awvalid(f[11]),  // input wire s_axi_awvalid
       .s_axi_awready(s_f[12]),  // output wire s_axi_awready
-      .s_axi_wdata(f[45:13]),      // input wire [31 : 0] s_axi_wdata
-      .s_axi_wstrb(f[50:46]),      // input wire [3 : 0] s_axi_wstrb
-      .s_axi_wvalid(f[51]),    // input wire s_axi_wvalid
-      .s_axi_wready(s_f[52]),    // output wire s_axi_wready
-      .s_axi_bresp(s_f[54:53]),      // output wire [1 : 0] s_axi_bresp
-      .s_axi_bvalid(s_f[55]),    // output wire s_axi_bvalid
-      .s_axi_bready(f[56]),    // input wire s_axi_bready
-      .s_axi_araddr(f[65:57]),    // input wire [8 : 0] s_axi_araddr
-      .s_axi_arvalid(f[66]),  // input wire s_axi_arvalid
-      .s_axi_arready(s_f[67]),  // output wire s_axi_arready
-      .s_axi_rdata(s_f[99:68]),      // output wire [31 : 0] s_axi_rdata
-      .s_axi_rresp(s_f[101:100]),      // output wire [1 : 0] s_axi_rresp
-      .s_axi_rvalid(s_f[102]),    // output wire s_axi_rvalid
-      .s_axi_rready(f[103]),    // input wire s_axi_rready
-      .sda_i(f[104]),                  // input wire sda_i
-      .sda_o(s_f[105]),                  // output wire sda_o
-      .sda_t(s_f[106]),                  // output wire sda_t
-      .scl_i(f[107]),                  // input wire scl_i
-      .scl_o(s_f[108]),                  // output wire scl_o
-      .scl_t(s_f[109]),                  // output wire scl_t
-      .gpo(s_f[110])                      // output wire [0 : 0] gpo
+      .s_axi_wdata(f[44:13]),      // input wire [31 : 0] s_axi_wdata
+      .s_axi_wstrb(f[48:45]),      // input wire [3 : 0] s_axi_wstrb
+      .s_axi_wvalid(f[49]),    // input wire s_axi_wvalid
+      .s_axi_wready(s_f[50]),    // output wire s_axi_wready
+      .s_axi_bresp(s_f[52:51]),      // output wire [1 : 0] s_axi_bresp
+      .s_axi_bvalid(s_f[53]),    // output wire s_axi_bvalid
+      .s_axi_bready(f[54]),    // input wire s_axi_bready
+      .s_axi_araddr(f[63:55]),    // input wire [8 : 0] s_axi_araddr
+      .s_axi_arvalid(f[64]),  // input wire s_axi_arvalid
+      .s_axi_arready(s_f[65]),  // output wire s_axi_arready
+      .s_axi_rdata(s_f[97:66]),      // output wire [31 : 0] s_axi_rdata
+      .s_axi_rresp(s_f[99:98]),      // output wire [1 : 0] s_axi_rresp
+      .s_axi_rvalid(s_f[100]),    // output wire s_axi_rvalid
+      .s_axi_rready(f[101]),    // input wire s_axi_rready
+      .sda_i(f[102]),                  // input wire sda_i
+      .sda_o(s_f[103]),                  // output wire sda_o
+      .sda_t(s_f[104]),                  // output wire sda_t
+      .scl_i(f[105]),                  // input wire scl_i
+      .scl_o(s_f[106]),                  // output wire scl_o
+      .scl_t(s_f[107]),                  // output wire scl_t
+      .gpo(s_f[108])                      // output wire [0 : 0] gpo
     );
 
     assign out = s_f[size - 1];
