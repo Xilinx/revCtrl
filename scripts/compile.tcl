2d2949f0cf32a135824d77cad43edb2a9b92014c
Greg Daughtry <greg.daughtry@xilinx.com>
2015-05-21 16:45:59 -0600

    updated compile script to write out bit file and SDK handoff files

--- scripts/compile.tcl
+++ scripts/compile.tcl
@@ -6,8 +6,14 @@ source -notrace $thisDir/utils.tcl
 # Create project
 open_project ./top/top.xpr
 
-launch_runs impl_1 -to_step write_bitstream
+#launch_runs impl_1 -to_step write_bitstream
+launch_runs impl_1 
 wait_on_run impl_1
+open_run impl_1
+
+write_bitstream top.bit
+write_hwdef -file top.hwdef
+write_sysdef -bitfile top.bit -hwdef top.hwdef -file top.sysdef
 
 # if everything is successful "touch" a file so make will not it's done
 touch {.compile.done}
