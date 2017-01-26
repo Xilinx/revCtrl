7bf49d14833aa4e0ae88bb7b62cd8b1f4aaacdea
Greg Daughtry <greg.daughtry@xilinx.com>
2017-01-26 15:08:59 -0700

    fixed problem with remote bd directory

--- scripts/bd_cip_2016_3.tcl
+++ scripts/bd_cip_2016_3.tcl
@@ -58,7 +58,7 @@ set run_remote_bd_flow 1
 if { $run_remote_bd_flow == 1 } {
   # Set the reference directory for source file relative paths (by default 
   # the value is script directory path)
-  set origin_dir ./work
+  set origin_dir .
 
   # Use origin directory path location variable, if specified in the tcl shell
   if { [info exists ::origin_dir_loc] } {
